97d6edb25fba0fe5a8f9201e4af526f11e48798a
Dominic <61340918+bolodave@users.noreply.github.com>
2020-05-26 16:46:18 -0700

    Created variables code
    Added

---
+++ pythonVariables.py
@@ -0,0 +1,21 @@
+import turtle
+
+"""
+Topic: Variables
+-They are reserved memory location to store values
+"""
+a = 5
+print(a)
+
+b = "hello"
+print(b)
+
+#Square
+#qazi_turtle = turtle.Turtle()
+#qazi_turtle.forward(100)
+#qazi_turtle.right(90)
+#qazi_turtle.forward(100)
+#qazi_turtle.right(90)
+#qazi_turtle.forward(100)
+#qazi_turtle.right(90)
+#qazi_turtle.forward(100)
