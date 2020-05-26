8f147d9e5769d7ad22ab9f05ad3b549acc37fa93
Dominic <61340918+bolodave@users.noreply.github.com>
2020-05-26 16:51:36 -0700

    Created functions tutorial file
    ///

---
+++ pythonFunctions.py
@@ -0,0 +1,20 @@
+import turtle
+
+"""
+Topic Functions:
+-Set of statements that take inputs, do something, and produces output
+-Functions we create are user-defined functions
+"""
+qazi_turtle = turtle.Turtle()
+def square():
+  qazi_turtle.forward(100)
+  qazi_turtle.right(90)
+  qazi_turtle.forward(100)
+  qazi_turtle.right(90)
+  qazi_turtle.forward(100)
+  qazi_turtle.right(90)
+  qazi_turtle.forward(100)
+
+square()
+qazi_turtle.forward(200)
+square()
