02bb1f96cc88bf40b5c7743f230258666073c3e7
repl_Dominic <replituser@example.com>
2020-05-27 01:47:35 +0000

    Added calendar python tutorial

---
+++ pythonCalendar.py
@@ -0,0 +1,29 @@
+import calendar
+
+"""
+
+"""
+print()
+print()
+
+"""
+print(calendar.weekheader(3)) #Outputs all the days of the week
+print()
+
+print(calendar.firstweekday())
+print()
+
+print(calendar.month(2020, 5))
+print()
+
+print(calendar.monthcalendar(2020, 5))
+print()
+
+print(calendar.calendar(2020))
+print()
+
+day_of_the_week = calendar.weekday(2020, 5, 26)
+print(day_of_the_week)
+"""
+is_leap = calendar.isleap(2019)
+print(is_leap)
